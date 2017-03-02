0603cb3e750a6790137d1d8539b8c814990635b4
Alexey Shalik <shalik2012@mail.ru>
2017-03-03 02:47:15 +0300

    added search for articles

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -4,11 +4,26 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SearchController extends Controller
 {
+    /**
+     * @Route("/search", name="user_search")
+     */
     public function searchAction(Request $request)
     {
-        return $this->render();
+        $articles = $this->get('search_article')->searchArticle($request);
+
+        $paginator  = $this->get('knp_paginator');
+        $content = $paginator->paginate(
+            $articles,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 3)
+        );
+
+        return $this->render('search.html.twig', array(
+            'content' => $content,
+        ));
     }
 }
--- src/AppBundle/Services/Search/SearchService.php
+++ src/AppBundle/Services/Search/SearchService.php
@@ -3,19 +3,26 @@
 namespace AppBundle\Services\Search;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\Container;
 
 class SearchService
 {
+    private $container;
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
     public function searchArticle(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery('
+        $em = $this->container->get('doctrine')->getManager()
+            ->createQuery('
         SELECT articles
         FROM AppBundle:Article articles 
-        WHERE articles.news LIKE :request;
-        ')->setParameter('request', $request);
+        WHERE articles.news LIKE :request
+        ')->setParameter('request', '%'.$request->query->get('search').'%');
 
-        $result = $query->getResult();
+        $result = $em->getResult();
 
         return $result;
     }
